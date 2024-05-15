9bde87e9be812e6f4d73d77352c69af538ef690f
Luka Panio <lukapanio@gmail.com>
2024-05-15 17:16:46 +0200

    Add static version.h
    Typically generated by meson

---
+++ src/lxc/version.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: LGPL-2.1+ */
+
+#ifndef __LXC_VERSION_H
+#define __LXC_VERSION_H
+
+#define LXC_DEVEL 1
+#define LXC_VERSION_MAJOR 6
+#define LXC_VERSION_MINOR 0
+#define LXC_VERSION_MICRO 0
+#define LXC_VERSION_ABI "1.8.0"
+#define LXC_VERSION "6.0.0-lindroid"
+
+#endif
